da9b6ab5fb65809d125cc41d5eff46e21a113921
Kota Ueshima <ueshima@usc.edu>
2023-02-21 13:38:50 -0800

    change URL for render

--- src/components/URL.js
+++ src/components/URL.js
@@ -1,8 +1,8 @@
-let URL;
-if (process.env.NODE_ENV === "development") {
-  URL = "http://localhost:3000";
+let URL
+if (process.env.NODE_ENV === 'development') {
+  URL = 'http://localhost:3000'
 } else {
-  URL = "https://globify-backend.herokuapp.com";
+  URL = 'https://globify-backend.onrender.com'
 }
 
-export default URL;
+export default URL
